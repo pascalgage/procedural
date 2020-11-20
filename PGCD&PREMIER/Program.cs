8db403adb628657223b851963b1a7006e26ddec5
pascalgage <72741160+pascalgage@users.noreply.github.com>
2020-11-20 22:17:05 +0100

    intitulé A et B

--- PGCD&PREMIER/Program.cs
+++ PGCD&PREMIER/Program.cs
@@ -18,7 +18,7 @@ namespace PGCD
 
                 //Entrez le premier entier positif
                 int A;
-                Console.WriteLine("Entrez un nombre entier A positif non nul:");
+                Console.WriteLine("Entrez un nombre entier A strictement positif non nul:");
                 A = int.Parse(Console.ReadLine());
 
                 bool test;
@@ -27,7 +27,7 @@ namespace PGCD
 
                 //Entrez le deuxième entier positif
                 int B;
-                Console.WriteLine("Entrez un nombre entier B positif non nul:");
+                Console.WriteLine("Entrez un nombre entier B strictement positif non nul:");
                 B = int.Parse(Console.ReadLine());
 
                 bool test2;
@@ -39,11 +39,26 @@ namespace PGCD
                 if (test == true)
                 {
                     Console.WriteLine("le nombre {0:#,###} est premier", A);
+                } 
+                
+                else if (A==0|A<0)
+                {
+                    Console.WriteLine("le nombre A N'EST PAS strictement positif ou non-nul !",A);
                 }
+                
+	
+	
                 if (test2 == true)
                 {
                     Console.WriteLine("le nombre {0:#,###} est premier", B);
+                } 
+                
+                else if (B==0|B<0)
+                {
+                    Console.WriteLine("le nombre B N'EST PAS strictement positif ou non-nul !",B);
                 }
+               
+	
 
                 do
                 {
