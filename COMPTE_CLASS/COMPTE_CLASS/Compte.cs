9d9dfcfc17791f5bf8039cc32468c71a1d9317fc
pascalgage <72741160+pascalgage@users.noreply.github.com>
2020-12-13 19:43:14 +0100

    Completed

--- COMPTE_CLASS/COMPTE_CLASS/Compte.cs
+++ COMPTE_CLASS/COMPTE_CLASS/Compte.cs
@@ -8,26 +8,60 @@ namespace COMPTE_CLASS
 {
     public class Compte
     {
+
+        //attributs....
         private int numeroCompte;
-        private int soldeCompte;
+        private double soldeCompte;
         private string nomTitulaire;
         private int decouvert;
+        
+        //propriétés....
+        public int UNnumeroCompte { get => numeroCompte; }
+        public double UNsoldeCompte { get => soldeCompte; set => soldeCompte=value; }
+        public string UNnomTitulaire { get => nomTitulaire; }
+        public int UNdecouvert { get => decouvert; }
 
-        public Compte(int UNnumeroCompte, int UNsoldeCompte, string UNnomTitulaire, int UNdecouvert)
+        //constructeur....
+        public Compte(int UNnumeroCompte, double UNsoldeCompte, string UNnomTitulaire, int UNdecouvert)
         {
             numeroCompte = UNnumeroCompte;
             soldeCompte = UNsoldeCompte;
             nomTitulaire = UNnomTitulaire;
             decouvert = UNdecouvert;
         }
-        public void SetNum(int value)
+        
+        //méthodes....
+        public void Crediter(double _montant)
         {
-            numeroCompte = value;
+            soldeCompte = soldeCompte + _montant;
+            
         }
+
+        public void Debiter(double _montant)
+        {
+            soldeCompte = soldeCompte - _montant;
+            if (soldeCompte<decouvert) {Console.WriteLine("Pas autorisé"); } else { Console.WriteLine("OK"); }
+        }
+
+        public void Transferer(double _montant,int UNnumeroCompte)
+        {
+            Console.WriteLine("Quel compte à créditer ?");
+            UNnumeroCompte = int.Parse(Console.ReadLine());
+
+            
+
+        }
+        public void Comparer()
+        {
+
+        }
+
 
         public string Infocompte()
         {
             return "N°:"+numeroCompte + " "+"Solde:" + soldeCompte + " "+"Nom:" + nomTitulaire + " " + decouvert+" euros";
         }
+
+
     }
 }
--- COMPTE_CLASS/COMPTE_CLASS/Program.cs
+++ COMPTE_CLASS/COMPTE_CLASS/Program.cs
@@ -10,10 +10,33 @@ namespace COMPTE_CLASS
     {
         static void Main(string[] args)
         {
+            //Outils de transfert (une variable)...
+            double _montant = 0;
+            int numeroCompte = 0;
+            
+
+            //Création des objets Compte bancaire.....
             Compte unCompte;
-            unCompte = new Compte(12345,3800,"gage",200);
+            unCompte = new Compte(12345,3800,"Gage Pascal",-200);
+
+            Compte deuxCompte;
+            deuxCompte = new Compte(54721, 1200, "Benitez Charles", -700);
+
+            //OPERATION DE TRANSFERT....
+
+            Console.WriteLine("Combien voulez-vous transférer ?");
+            _montant = double.Parse(Console.ReadLine());
+
+
+            unCompte.Debiter(_montant);
+            deuxCompte.Crediter(_montant);
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////
+            
+            unCompte.Transferer(_montant,numeroCompte);
 
             Console.WriteLine(unCompte.Infocompte());
+            Console.WriteLine(deuxCompte.Infocompte());
 
             Console.ReadKey();
         }
