790a4a006c78a684f718378b6960e352d0f6ab31
pascalgage <72741160+pascalgage@users.noreply.github.com>
2020-12-17 23:23:29 +0100

    Fraction en cours...

---
+++ FRACTION_CLASS/FRACTION_CLASS/Fraction.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FRACTION_CLASS
+{
+    public class Fraction
+    {
+        //Attributs....
+        private int numerateur;
+        private int denominateur;
+
+        //propriétés....
+        public int UNnumerateur { get => numerateur; set => numerateur = value; }
+        public int UNdenominateur { get=> denominateur; set=> denominateur =value; }
+        
+        //constructeur....
+        public Fraction(int UNnumerateur, int UNdenominateur)
+        {
+            this.numerateur = UNnumerateur;
+            this.denominateur = UNdenominateur;
+            
+        }
+        //constructeur à vide....
+        public Fraction()
+        {
+            this.numerateur = 0;
+            this.denominateur = 0;
+
+           
+        }
+        //méthodes....
+        public int Infos()
+        {
+            int resultat;
+            return resultat= numerateur/denominateur; 
+        }
+        public override string ToString()
+        {
+            return +numerateur+"/"+denominateur;
+        }
+        public void Oppose(int numerateur,int denominateur)
+        {
+            this.numerateur = -(numerateur);
+        }
+        public void Inverse(int numerateur, int denominateur)
+        {
+            this.numerateur = denominateur;
+            this.denominateur = numerateur;
+        }
+        public bool Superieur( Fraction _F)
+        {
+            if ((this.numerateur/this.denominateur) > (_F.numerateur / _F.denominateur))
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("est supérieur !!!");
+                return true;
+            }
+        }
+
+
+
+
+    }
+}
---
+++ FRACTION_CLASS/FRACTION_CLASS/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FRACTION_CLASS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction F1;
+            F1 = new Fraction(5, 2);
+
+            Fraction F2;
+            F2 = new Fraction(10, 7);
+
+            Fraction F3;
+            F3 = new Fraction(0, 1);
+
+            
+            
+
+            F1.Superieur(F2);
+
+            Console.WriteLine(F2.Infos());
+            Console.WriteLine(F3.Infos());
+
+
+            Console.WriteLine(F1);
+            Console.WriteLine(F2);
+
+
+            Console.ReadLine();
+        }
+    }
+}
