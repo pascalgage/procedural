4a5abb302f1a86cb915e5d643b801a5881fd8d05
Gage <72741160+pascalgage@users.noreply.github.com>
2020-12-22 02:07:54 +0100

    MAJ 2.07AM
    MAJ OK

--- FRACTION_CLASS/FRACTION_CLASS/Program.cs
+++ FRACTION_CLASS/FRACTION_CLASS/Program.cs
@@ -33,22 +33,26 @@ namespace FRACTION_CLASS
             Fraction F2;
             F2 = new Fraction(M, E);
 
-            Console.WriteLine("L'addition de "+F1.ToString()+" et de "+F2.ToString()+" est  égale à: "); F1.Plus(F2);
+            Console.WriteLine("L'addition de "+F1.ToString()+" et de "+F2.ToString()+" est égale à: "); F1.Plus(F2);
+            Console.WriteLine(" ");
             Console.WriteLine("La soustraction des deux fractions est égale à: "); F1.Moins(F2);
+            Console.WriteLine(" ");
             Console.WriteLine("La multiplication des deux fractions est égale à: "); F1.Multiplie(F2);
+            Console.WriteLine(" ");
             Console.WriteLine("La division des deux fractions est égale à: "); F1.Divise(F2);
-            
+            Console.WriteLine(" ");
             F1.Superieur(F2);
             F1.Inferieur(F2);
             F1.Egal(F2);
-
-            Console.WriteLine("La fraction " + F1.ToString() + " est égale à:");
+            Console.WriteLine(" ");
+            Console.Write("La fraction " + F1.ToString() + " est égale à: ");
             F1.Infos(N,D);
-            Console.WriteLine("La fraction " + F2.ToString() + " est égale à:");
+            Console.Write("La fraction " + F2.ToString() + " est égale à: ");
             F2.Infos(M,E);
-            Console.WriteLine("L'opposé de la fraction " + F1.ToString() + " est:");
+            Console.WriteLine(" ");
+            Console.Write("L'opposé de la fraction " + F1.ToString() + " est: ");
             F1.Oppose();
-            Console.WriteLine("L'opposé de la fraction " + F2.ToString() + " est:");
+            Console.Write("L'opposé de la fraction " + F2.ToString() + " est: ");
             F2.Oppose();
 
             
