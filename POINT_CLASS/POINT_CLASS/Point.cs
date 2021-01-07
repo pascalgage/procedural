23f06ed404dfb2cc0a85a9877db255c35b97850e
Gage <72741160+pascalgage@users.noreply.github.com>
2021-01-07 16:16:08 +0100

    Le point

---
+++ POINT_CLASS/POINT_CLASS/Point.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POINT_CLASS
+{
+    public class Point
+    {
+        //attributs...
+        private float axeAbscisseX;
+        private float axeOrdonneeY;
+        //propriétés...
+        public float UNaxeAbscisseX { get => axeAbscisseX; set => axeAbscisseX = value; }
+        public float UNaxeOrdonneeY { get => axeOrdonneeY; set => axeOrdonneeY = value; }
+        //constructeur...
+        public Point(float UNaxeAbscisseX,float UNaxeOrdonneeY)
+        {
+            this.axeAbscisseX = UNaxeAbscisseX;
+            this.axeOrdonneeY = UNaxeOrdonneeY;
+        }
+        public Point()
+        {
+            this.axeAbscisseX = 0;
+            this.axeOrdonneeY = 0;
+
+        }
+        //méthodes...
+        public void Deplacer(float _X,float _Y)
+        {
+            this.axeAbscisseX = _X;
+            this.axeOrdonneeY = _Y;
+        }
+        public override string ToString()
+        {
+            return "Abscisse est: " + axeAbscisseX + " Ordonnee est: " + axeOrdonneeY;
+        }
+        public void ConstruireSymOrd(float _X,float _Y)
+        {
+            this.axeAbscisseX =  (_X);
+            this.axeOrdonneeY = -(_Y);
+        }
+        public void ConstruireSymAbs(float _X,float _Y)
+        {
+            this.axeAbscisseX = -(_X);
+            this.axeOrdonneeY =  (_Y);
+        }
+        public void ConstruireSymOr(float _X,float _Y)
+        {
+            this.axeAbscisseX = -(_X);
+            this.axeOrdonneeY = -(_Y);
+        }
+
+        
+        
+
+        
+    }
+}
---
+++ POINT_CLASS/POINT_CLASS/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POINT_CLASS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point pointA;
+            pointA = new Point(0, 0);
+
+            Point pointB;
+            pointB = new Point(1, 1);
+
+            pointA.Deplacer(3, 4);
+            pointB.Deplacer(7, 4);
+
+            
+
+            Console.WriteLine(pointA.ToString());
+            Console.WriteLine(pointB.ToString());
+
+
+            Console.ReadKey();
+        }
+    }
+}
