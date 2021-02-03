d993c042402422d571e952ab7ef0b18fece32b19
Gage <72741160+pascalgage@users.noreply.github.com>
2021-02-03 19:59:40 +0100

    MAJ photos
    CSS +photos

---
+++ MENU deroulant TRUITE/js/prog2.js
@@ -0,0 +1,6 @@
+var btn = document.querySelector('.button2');
+var nav2 = document.querySelector('.niveau3');
+
+btn.onclick = function(){
+	nav2.classList.toggle('niveau3');
+}
---
+++ MENU deroulant TRUITE/js/prog3.js
@@ -0,0 +1,6 @@
+var btn = document.querySelector('.button3');
+var nav3 = document.querySelector('.niveau4');
+
+btn.onclick = function(){
+	nav3.classList.toggle('niveau4');
+}
