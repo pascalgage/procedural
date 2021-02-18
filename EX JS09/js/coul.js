c45635b409c4c09c65a42370bb003e414895683b
Gage <72741160+pascalgage@users.noreply.github.com>
2021-02-18 19:53:10 +0100

    modifications du 18/02
    Retrait du JS dans le HTML....

--- EX JS09/js/coul.js
+++ EX JS09/js/coul.js
@@ -1,33 +1,49 @@
-
-
-
-function change(){
-	var color=document.querySelector("#A").value;
-	document.body.style.background = color;
-}
-
-var test=document.querySelector("#A");
-test.addEventListener("blur",change);
-
-
-function changeColor(color) { 
-                document.body.style.background = color; 
-            }
-
 function changing(){
 
-var color ='#'+(document.querySelector("#A").value)+(document.querySelector("#B").value)+(document.querySelector("#C").value);
+var color ='#'+(document.querySelector("#A").value)+
+(document.querySelector("#B").value)+
+(document.querySelector("#C").value);
+color=color.toUpperCase();
 document.body.style.background = color;
-
 }
-
-var test2=document.querySelector("#C");
+var test1=document.querySelector("#C");
+test1.addEventListener("blur",changing);
+var test2=document.querySelector("#A");
 test2.addEventListener("blur",changing);
+var test3=document.querySelector("#B");
+test3.addEventListener("blur",changing);
+
+function changeRED(){
+	var color='red';
+	document.body.style.background = color;
+	document.querySelector("#A").value="FF";
+	document.querySelector("#B").value="00";
+	document.querySelector("#C").value="00";
+}
+var red=document.querySelector("#red");
+red.addEventListener("click",changeRED);
 
 
+function changeGREEN(){
+	var color='green';
+	document.body.style.background = color;
+	document.querySelector("#A").value="00";
+	document.querySelector("#B").value="80";
+	document.querySelector("#C").value="00";
+}
+var green=document.querySelector("#green");
+green.addEventListener("click",changeGREEN);
 
 
-
+function changeBLUE(){
+	var color='blue';
+	document.body.style.background = color;
+	document.querySelector("#A").value="00";
+	document.querySelector("#B").value="00";
+	document.querySelector("#C").value="FF";
+}
+var blue=document.querySelector("#blue");
+blue.addEventListener("click",changeBLUE);
 
 
 
