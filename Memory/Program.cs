e4556e9aaa57a6fc99d2a4ab4b975598cb5049f5
Gage <72741160+pascalgage@users.noreply.github.com>
2021-05-24 22:52:15 +0200

    ajout memory
    le plus simple est parfois difficile à trouver !!!

---
+++ Memory/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int nbr;
+            double r=4;
+
+            Console.WriteLine("Combien de mémoire voulez-vous ?");
+            nbr = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= nbr; i++)
+            {
+                
+                r = (r*2);
+                Console.WriteLine(r);
+
+                
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
